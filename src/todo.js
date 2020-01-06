3611fdaba80fc98f2e247dc2e057a6c8296fd1b2
kohei.kubota <kohei.kubota@koheikubotanoMacBook-Pro.local>
2020-01-06 16:58:08 +0900

    create layout

---
+++ src/todo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './css/index.css';
+
+function Todo() {
+  return (
+    <div className='wrap'>
+      <div className='side-area'></div>
+      <div className='main-area'>
+        <header className='header'></header>
+        <footer className='footer'></footer>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
