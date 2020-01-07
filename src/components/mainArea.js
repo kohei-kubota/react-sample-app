ad990d5b3d8d39d2fdafb8313ecd208aa5e09b36
kohei.kubota <kohei.kubota@koheikubotanoMacBook-Pro.local>
2020-01-07 15:06:44 +0900

    create todo-item

--- src/components/mainArea.js
+++ src/components/mainArea.js
@@ -4,9 +4,24 @@ import Footer from './footer';
 
 class Mainarea extends Component {
   render() {
+    const todos = [
+      { label: 'Todo1' },
+      { label: 'Todo2' },
+    ]
+
+    let todoItemDom = [];
+    for (var i = 0; i < todos.length; i++) {
+      let todoItem = <li className='todo-list-item' key={'item-' + i}>{todos[i].label}></li>;
+      todoItemDom.push(todoItem);
+    }
     return (
       <div className='main-area'>
         <Header />
+        <main className='list-area'>
+          <ul className='todo-list'>
+            {todoItemDom}
+          </ul>
+        </main>
         <Footer />
       </div>
     );
