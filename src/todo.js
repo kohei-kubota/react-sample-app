4c7f163b729c5456ab76e4856c110aa755f43de5
kohei.kubota <kohei.kubota@koheikubotanoMacBook-Pro.local>
2020-01-07 11:39:57 +0900

    create component

--- src/todo.js
+++ src/todo.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './css/index.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className='header'>受信箱</header>
+    );
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className='footer'>&copy;2019 Aire Project</footer>
+    );
+  }
+}
+
+class Sidearea extends Component {
+  render() {
+    return (
+      <div className='side-area'></div>
+    );
+  }
+}
+
+class Mainarea extends Component {
+  render() {
+    return (
+      <div className='main-area'>
+        <Header />
+        <Footer />
+      </div>
+    );
+  }
+}
 
 function Todo() {
   return (
     <div className='wrap'>
-      <div className='side-area'></div>
-      <div className='main-area'>
-        <header className='header'></header>
-        <footer className='footer'></footer>
-      </div>
+      <Sidearea />
+      <Mainarea />
     </div>
   );
 }
